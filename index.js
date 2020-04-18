fd180e4c2cb8df5aae5b105c67725ca3d481cc9d
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-18 00:19:33 -0400

    cleaned up index

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-// const Transcript = require('./src/api/Transcript')
-// const Model = require('./src/api/Model')
-// const Upload = require('./src/api/Upload')
-// const Client = require('./src/Client')
-
-// module.exports = {
-//   Transcript,
-//   Model,
-//   Upload,
-//   setAPIKey (key) {
-//     Client.API_KEY = key
-//   }
-// }
-
-
-
-
 const assemblyai = require('./index.js');
+
 module.exports = {
   assemblyai
 }
