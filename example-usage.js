f79aa1aada0f8b292c6e621813f54836e82bd431
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-10-06 16:59:07 -0400

    tweak

--- example-usage.js
+++ example-usage.js
@@ -1,22 +1,24 @@
-require('dotenv').config()
+require('dotenv').config();
+const fs = require('fs');
 const assemblyai = require('./index.js');
 const ApiKey = process.env.ASSEMBLYAI_API_KEY;
 
-const filePath = __dirname+"/example.mp3"; // some path to a local file, in .gitignore 
+const filePath = __dirname + '/example.mp3'; // some path to a local file, in .gitignore
 
 async function upload() {
-    try {
-        const response = await assemblyai({
-            ApiKey, 
-            filePath,
-            languageModel: 'assemblyai_media',
-            acousticModel: 'assemblyai_default'
-        });
-        console.log('response', response)
-    } catch (e) { // Do some error handling here
-        console.log('error in example usage:: ', e)
-    }
+  try {
+    const response = await assemblyai({
+      ApiKey,
+      filePath,
+      languageModel: 'assemblyai_media',
+      acousticModel: 'assemblyai_default',
+    });
+    console.log('response', response);
+    fs.writeFileSync(__dirname + '/example-speakers.json', JSON.stringify(response, null, 2));
+  } catch (e) {
+    // Do some error handling here
+    console.log('error in example usage:: ', e);
+  }
 }
 
 upload();
-
